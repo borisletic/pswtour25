675d7ef7ded7c63087728c36cdf3902d8fa9c679
borisletic <borisletic001@gmail.com>
2025-07-12 02:02:55 +0200

    fun: added exit festival

--- backend/TourApp.Infrastructure/obj/Debug/net8.0/TourApp.Infrastructure.AssemblyInfo.cs
+++ backend/TourApp.Infrastructure/obj/Debug/net8.0/TourApp.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+165cfae406b6bbdcdd72d220abaf4726a414efe5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d3d09fc2f10dc28d08232552cc89770919f4894")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
